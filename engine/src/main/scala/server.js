43abc71c8891d0c6e84be2ffc81203ebc18466fc
Samora Njozela <sizovikela@gmail.com>
2013-12-08 18:56:02 +0200

    added intial server.js

---
+++ engine/src/main/scala/server.js
@@ -0,0 +1,14 @@
+var vertx = require('vertx');
+var server = vertx.createHttpServer();
+var routeMatcher = new vertx.RouteMatcher();
+
+
+routeMatcher.get('/test1', function(req){
+	req.response.end("test1 returned");
+	});
+	
+routeMatcher.get('/test2', function(req){
+	req.response.end("test2 returned");
+	});	
+
+server.requestHandler(routeMatcher).listen(8080, 'localhost/aws');
