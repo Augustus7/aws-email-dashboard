4ca030871b6042edc42564b0711a84edc87b504d
Samora Njozela <sizovikela@gmail.com>
2014-01-30 03:20:02 +0200

    able to interface with mongoDB module

--- engine/src/main/resources/server.js
+++ engine/src/main/resources/server.js
@@ -1,7 +1,57 @@
 var vertx = require('vertx');
 var server = vertx.createHttpServer();
 var routeMatcher = new vertx.RouteMatcher();
+var container = require('vertx/container');
+var eb = vertx.eventBus;
+var console = require('vertx/console');	
 
+var config = {
+    "address": "test.my_persistor",
+    "host": "localhost",
+    "port": 27017,
+    "pool_size": 3,
+    "db_name": "vertx"
+}
+
+var findZipsAL = {
+	"action" : "find",
+	"collection" : "zips",
+	"matcher" : {
+			"state" : "AL"
+	},
+	"limit" : 3
+}
+
+var listbounces = {
+	"action" : "find",
+	"collection" : "bounced_emails",
+	"matcher" : {}
+}
+
+var testJson = {
+				"date" : "2014-01-30",
+				"to" : "samora@enetelct.co.za",
+				"from" : "kebayes@entelect.co.za"	
+}
+var testJson2 = {
+				"date" : "2015-01-30",
+				"to" : "samora@enetelct.co.za",
+				"from" : "kebayes@entelect.co.za"	
+}
+
+
+
+container.deployModule("io.vertx~mod-mongo-persistor~2.0.0-final", config, function(err){
+	if(!err){
+		console.log("deployed");
+		//Used for subscription to queue where JSon string of bounced email details will be published
+		eb.registerHandler('camel.queue', handleBouncedEmail);
+		//test the subscription on this address
+		eb.publish('camel.queue', testJson);
+		eb.publish('camel.queue', testJson2);
+		}
+	else console.log(err);
+	});
 
 routeMatcher.post('/api/:version/aws/ses/bounce', function(request) {
 	var apiVersion = req.params().get('version');
@@ -14,16 +64,46 @@ routeMatcher.post('/api/:version/aws/ses/bounce', function(request) {
 
 });
 
-routeMatcher.noMatch(function(request) {
-    req.response().end('What are you trying to do?');
+routeMatcher.noMatch(function(req) {
+    req.response.end('What are you trying to do?');
 });
 
-routeMatcher.get('/test1', function(req){
-	req.response.end("test1 returned");
+routeMatcher.get('/list/all', function(req){
+	eb.send("test.my_persistor", findZipsAL, function(reply){
+			req.response.end(JSON.stringify(reply, null, "\t"));
+		});
+	
 });
 	
-routeMatcher.get('/test2', function(req){
-	req.response.end("test2 returned");
-	});	
+routeMatcher.get('/list/bounces', function(req){
+		eb.send("test.my_persistor", listbounces, function(reply){
+			req.response.end(JSON.stringify(reply, null, "\t"));
+		});
+	});
 
+
+var handleBouncedEmail = function(message){
+	console.log("method called");
+	var body = eval(message);
+	var SaveQuery = {
+		"action" : "save",
+		"collection" : "bounced_emails",
+		"document" : {
+				"date" : body.date,
+				"to" : body.to,
+				"from" : body.from
+			}
+		} 	
+	eb.send(
+		"test.my_persistor", 
+		SaveQuery, 
+		function(reply){
+			console.log(JSON.stringify(reply, null, "\t"));
+		});
+	}
+		
 server.requestHandler(routeMatcher).listen(8080, 'localhost');
+
+
+
+
